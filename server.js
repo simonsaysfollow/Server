214bd089074ccfdd777d99edf57a668d6947687b
Simon Tekeste <st.tekeste@gmail.com>
2018-12-22 19:29:17 -0800

    still working on the debugging

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(logger);
 
 // grabs random data from from mlab data base
 
-app.get("/", (request, response) => {
+app.get("https://nameless-plateau-88597.herokuapp.com/", (request, response) => {
     console.log("s")
     db.collection('datas')
         .aggregate([{ $sample: { size: 1 } }])
@@ -37,7 +37,7 @@ app.get("/", (request, response) => {
 
 
 //adds data to mlab db
-app.post('/data/', (request) => {
+app.post('https://nameless-plateau-88597.herokuapp.com/data/', (request) => {
    
     const data = request.body;
     let mods = [];
@@ -55,7 +55,7 @@ app.post('/data/', (request) => {
 });
 
 //updates db for items swiped right(good)
-app.post("/change/",(request,response)=>{
+app.post("https://nameless-plateau-88597.herokuapp.com/change/",(request,response)=>{
     console.log("are you entering?")
     let info = request.body;
     let idString = info['id']; 
@@ -65,7 +65,7 @@ app.post("/change/",(request,response)=>{
 
 })
 // return list of all items with liked == true
-app.get("/truevalues/",(request,response)=>{
+app.get("https://nameless-plateau-88597.herokuapp.com/truevalues/",(request,response)=>{
     db.collection('datas')
         //.find({"liked":true })
         .aggregate([
